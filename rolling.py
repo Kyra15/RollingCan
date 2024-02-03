7516bc3d490432e7474512144a9d9c13490d6aa2
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-02-02 23:56:47 -0500

    Update rolling.py

--- rolling.py
+++ rolling.py
@@ -2,69 +2,87 @@
 import cv2
 import numpy as np
 
+
+# apply all the preliminary filters (grayscale, gaussian blue, and canny edge)
 def testdect(pic):
-    # mask = np.zeros(pic.shape[:2], dtype="uint8")
-    # cv2.circle(mask, (500, 400), 500, 255, -1)
-    # masked = cv2.bitwise_and(pic, pic, mask=mask)
-    bw_pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
-    gauss_bw_pic = cv2.GaussianBlur(bw_pic, (15, 15), 0)
-    canny_gauss_bw_pic = cv2.Canny(gauss_bw_pic, 50, 70)
-    return canny_gauss_bw_pic
+    gauss_pic = cv2.GaussianBlur(pic, (15, 15), 0)
+    bw_gauss_pic = cv2.cvtColor(gauss_pic, cv2.COLOR_BGR2GRAY)
+    canny_gauss_bw_pic = cv2.Canny(bw_gauss_pic, 50, 70)
+    return pic, canny_gauss_bw_pic
 
 
-def detection(pic):
-    # convert to grayscale and gaussian blur
-    # mask = np.zeros(pic.shape[:2], dtype="uint8")
-    # cv2.circle(mask, (500, 500), 400, 255, -1)
-    # masked = cv2.bitwise_and(pic, pic, mask=mask)
-    bw_pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
-    gauss_bw_pic = cv2.GaussianBlur(bw_pic, (15, 15), 0)
-    canny_gauss_bw_pic = cv2.Canny(gauss_bw_pic, 50, 70)
-
-    # use the cv function called HoughCircles to detect circles and output those lines
-    detected_circles = cv2.HoughCircles(canny_gauss_bw_pic, cv2.HOUGH_GRADIENT, 1, 20,
-                                        param1=50, param2=20, minRadius=100,
-                                        maxRadius=400)
+# detect and output the cricles detected using open cv's HoughCircles
+def detection(og, pic):
+    # detect the circles with the HoughCircles function
+    detected_circles = cv2.HoughCircles(pic, cv2.HOUGH_GRADIENT, 1, 20,
+                                        param1=50, param2=10, minRadius=275,
+                                        maxRadius=300)
 
     # make sure at least some circles were detected
     if detected_circles is not None:
         detected_circles = np.uint16(np.around(detected_circles))
 
         # get the first parameters from the circle that is detected
-        first_circle = detected_circles[0][1]
+        first_circle = detected_circles[0][0]
 
         # get the parameters for the center coordinates
         # and the radius of the first circle detected
         x, y, r = first_circle[0], first_circle[1], first_circle[2]
 
         # draw the circle for the circumference of the can
-        cv2.circle(pic, (x, y), r, (70, 255, 133), 16)
+        cv2.circle(og, (x, y), r, (70, 255, 133), 16)
 
         # draw the smaller circle for the center of the circle
-        cv2.circle(pic, (x, y), 1, (70, 255, 133), 18)
+        cv2.circle(og, (x, y), 1, (70, 255, 133), 18)
 
-        return pic
+        return og
     else:
+        # if no circles are detected print "no circle" in the console and return the original image
         print("no circle")
-        return pic
+        return og
+
+
+# function for saving the final overlayed video
+def writeVideo():
+    # get the screen resolution of the frame
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+
+    size = (frame_width, frame_height)
+
+    # use theVideoWriter function to create a new video file
+    # with the previously obtained screen resolution
+    final = cv2.VideoWriter('writtenvid.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
+    return final
 
 
 # create video capture object
 video = cv2.VideoCapture("rollingcan4.mov")
 
+# this line calls the function for saving the file
+final = writeVideo()
+
 while video.isOpened():
+    # read each frame
     ret, frame = video.read()
     if ret:
-        test = testdect(frame)
-        cv2.imshow("Test", test)
-        detected_frame = detection(frame)
+        # call the function that applies filters on the frame
+        frame, test = testdect(frame)
+
+        # this line creates the actual file for the new video
+        final.write(detection(frame, test))
+
+        # detect and draw the Hough Circles by calling the detection function
+        detected_frame = detection(frame, test)
+
+        # show the frame
         cv2.imshow("Detected Frame", detected_frame)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
         break
 
-
+# stop the 2 video capture objects and destroy all of the open cv windows
 video.release()
+final.release()
 cv2.destroyAllWindows()
-
